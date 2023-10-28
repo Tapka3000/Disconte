70272b9cb17d79b053ae30fd65f0d4801c6d8aa6
Tapka3000 <circle.table@yandex.ru>
2023-10-28 20:15:25 +0300

    Commit

--- src/main/java/org/example/Disconte.java
+++ src/main/java/org/example/Disconte.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Disconte {
     public static void main(String[] args) {
-        int costShirt = 1500;
+        int costShirt = 1500; // стоимость без скидки
         int quantityShirt = 2;
         int costDiscount = 3;
         int quantityDiscont = 1;
@@ -13,7 +13,19 @@ public class Disconte {
         double oneShirt = oneShirtPrice((int) priceDisc);
         double economy = economyCash(costShirt, quantityShirt, (int) oneShirt);
         double freebie = freebieShirt((int) economy, costShirt);
-        System.out.println(freebie);
+
+        System.out.println("Стоимость футболки без скидки " + costShirt + " рублей");
+        System.out.println("Выбрано рубашек " + quantityShirt);
+        System.out.println("Необходимое количество рубашек, для получения скидки " + costDiscount);
+        System.out.println("Количество рубашек, которые надо добавить для получения скидки " + quantityDiscont);
+        System.out.println("Цена выбранных рубашек без скидки " + totalPriceWithoundDiscont + " рублей");
+        System.out.println("Сумма, которую необходимо потратить для добавления рубашек, что бы получить скидку " + remainingQuantity + " рублей");
+        System.out.println("Скидка " + discount + " %");
+        System.out.println("Цена за 3 рубашки со скидкой " + priceDisc + " рублей");
+        System.out.println("Цена за 1 рубашку со скидкой " + oneShirt + " рублей");
+        System.out.println("Экономия, если купить 3 рубашки со скидкой, вместо 2-х без скидки " + economy + " рублей");
+
+        System.out.println("Количество халявных рубашек, купленных на сэкономленные деньги " + freebie + " шт.");
     }
 
     // Цена за 3 рубашки со скидкой
